f967fe331bdd14f1967dd0d8cf4141f3b5459efe
SL477 <fishwickt@gmail.com>
2019-06-12 20:26:59 +0100

    challenge 1

--- myApp.js
+++ myApp.js
@@ -7,11 +7,12 @@
 /*  ================== */
 
 /** 1) Install & Set up mongoose */
-
+require('dotenv').config();
 // Add `mongodb` and `mongoose` to the project's `package.json`. Then require 
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
-
+const mongoose = require('mongodb').MongoClient;
+mongoose.connect(process.env.MONGO_URI);
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
